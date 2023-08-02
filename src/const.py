42de1e8de319ff326533c9cca3216390451eeee1
Niko10Z <nikikozey@gmail.com>
2023-08-02 14:06:01 +0300

    test commit from desctop

--- src/const.py
+++ src/const.py
@@ -2,5 +2,5 @@ import os
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))[:-4]
-conf_log_filename = os.path.join(ROOT_DIR, 'pars_log.txt')
+conf_log_filename = os.path.join(ROOT_DIR, 'pars_log.log')
 conf_last_parsing_dt_filename = os.path.join(ROOT_DIR, 'last_parsing.txt')
